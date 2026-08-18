78570def6ea5b141fc988c764950995abb62bba0
Suraj Vijay <surajcpp115@gmail.com>
2026-08-18 12:29:34 +0530

    Add initial HTML file and update main.go for file serving

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import (
 	"net/http"
 )
 
-const port = "8080"
+const (
+	port         = "8080"
+	filePathRoot = "."
+)
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
