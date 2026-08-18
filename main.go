42830f7f527a15d68e751c5f10d4bcb4030fcb37
Suraj Vijay <surajcpp115@gmail.com>
2026-08-18 12:52:55 +0530

    Add logo image and update main.go for improved routing and health check

--- main.go
+++ main.go
@@ -12,7 +12,13 @@ const (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	server := &http.Server{
 		Handler: mux,
