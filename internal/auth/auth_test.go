530e903ab8000b002cfcae57836b08f76def18c3
Suraj Vijay <surajcpp115@gmail.com>
2026-08-23 00:44:18 +0530

    Refactor password hash testing to improve structure and coverage

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,31 +1,69 @@
 package auth
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestCheckPasswordHashWithActualPwdAndHash(t *testing.T) {
-	pwd := "thiS$is^mY&$tr0nG_pwD"
+func TestCheckPasswordHash(t *testing.T) {
+	// First, we need to create some hashed passwords for testing
+	password1 := "correctPassword123!"
+	password2 := "anotherPassword456!"
+	hash1, _ := HashPassword(password1)
+	hash2, _ := HashPassword(password2)
 
-	pwdHash, err := HashPassword(pwd)
-	if err != nil {
-		t.Errorf(`failed to get hash for given password - "%s", error: %v`, pwd, err)
+	tests := []struct {
+		name          string
+		password      string
+		hash          string
+		wantErr       bool
+		matchPassword bool
+	}{
+		{
+			name:          "Correct password",
+			password:      password1,
+			hash:          hash1,
+			wantErr:       false,
+			matchPassword: true,
+		},
+		{
+			name:          "Incorrect password",
+			password:      "wrongPassword",
+			hash:          hash1,
+			wantErr:       false,
+			matchPassword: false,
+		},
+		{
+			name:          "Password doesn't match different hash",
+			password:      password1,
+			hash:          hash2,
+			wantErr:       false,
+			matchPassword: false,
+		},
+		{
+			name:          "Empty password",
+			password:      "",
+			hash:          hash1,
+			wantErr:       false,
+			matchPassword: false,
+		},
+		{
+			name:          "Invalid hash",
+			password:      password1,
+			hash:          "invalidhash",
+			wantErr:       true,
+			matchPassword: false,
+		},
 	}
 
-	isValid, err := CheckPasswordHash(pwd, pwdHash)
-	if !isValid || err != nil {
-		t.Errorf(`CheckPasswordHash("%s", "%s"); expected: true, actual: false`, pwd, pwdHash)
-	}
-
-}
-func TestCheckPasswordHashWithDifferentPwdAndHash(t *testing.T) {
-	pwd := "thiS$is^mY&$tr0nG_pwD"
-
-	pwdHash, err := HashPassword(pwd)
-	if err != nil {
-		t.Errorf(`failed to get hash for given password - "%s", error: %v`, pwd, err)
-	}
-
-	isValid, err := CheckPasswordHash(pwd+"abc", pwdHash)
-	if isValid || err != nil {
-		t.Errorf(`CheckPasswordHash("%s", "%s"); expected: false, actual: true`, pwd+"abc", pwdHash)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			match, err := CheckPasswordHash(tt.password, tt.hash)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CheckPasswordHash() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !tt.wantErr && match != tt.matchPassword {
+				t.Errorf("CheckPasswordHash() expects %v, got %v", tt.matchPassword, match)
+			}
+		})
 	}
 }
