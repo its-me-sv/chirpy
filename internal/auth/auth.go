c471790d22b73604922dff8e182a1f2e9d1a3be6
Suraj Vijay <surajcpp115@gmail.com>
2026-08-23 13:29:46 +0530

    Refactor JWT issuer handling to use a constant and improve error handling

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -9,6 +9,8 @@ import (
 	"github.com/google/uuid"
 )
 
+const tokenIssuer = "chirpy-access"
+
 func HashPassword(password string) (string, error) {
 	return argon2id.CreateHash(password, argon2id.DefaultParams)
 }
@@ -19,7 +21,7 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 
 func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
 	return jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
-		Issuer:    "chirpy-access",
+		Issuer:    tokenIssuer,
 		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
 		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
 		Subject:   userID.String(),
@@ -34,12 +36,20 @@ func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
 		return []byte(tokenSecret), nil
 	})
 	if err != nil {
-		return uuid.UUID{}, err
+		return uuid.Nil, err
+	}
+
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if issuer != tokenIssuer {
+		return uuid.Nil, fmt.Errorf("invalid issuer")
 	}
 
 	userID, err := token.Claims.GetSubject()
 	if err != nil {
-		return uuid.UUID{}, err
+		return uuid.Nil, err
 	}
 
 	return uuid.Parse(userID)
